51910683b3768610630c8ec4f4599a0879d16e1d
rishabhbizzle <rishabhbizzle@gmail.com>
2024-02-14 23:39:43 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+import { ThemeProvider } from './components/theme-provider'
+import { ModeToggle } from './components/mode-toggle'
+import Home from './pages/home'
+import LandingPage from './pages/landing-page'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ThemeProvider defaultTheme = "dark"
+    storageKey = "react">
+      {/* <LandingPage/> */}
+    <Home />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/pages/landing-page.jsx
@@ -0,0 +1,339 @@
+import { Button } from "@/components/ui/button"
+import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Input } from "@/components/ui/input"
+import { SignInButton } from "@clerk/clerk-react"
+
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen">
+      <nav className="flex justify-between items-center py-4 px-8 border-b border-gray-800">
+        <div>
+            RR
+        </div>
+        <div className="flex items-center space-x-4">
+        <SignInButton />
+        </div>
+      </nav>
+      <header className="flex text-center py-24 h-screen">
+        <h1 className="text-8xl font-bold mb-6">Your fastest path to production</h1>
+        <p className="text-xl text-gray-400 mb-8">
+          Build, deploy your React application with just one click
+        </p>
+        <div className="space-x-4">
+          <Button className="">Get Started for Free</Button>
+          <Button variant="outline">Contact Sales</Button>
+        </div>
+      </header>
+      {/* <section className=" py-16">
+        <div className="text-center mb-12">
+          <h2 className="text-4xl font-bold mb-4">Whatever your stack, it runs on Render.</h2>
+          <div className="flex justify-center space-x-4 flex-wrap">
+            <LanguagesIcon className="text-[#00ADD8] h-12" />
+            <GemIcon className="text-[#CC342D] h-12" />
+            <CodepenIcon className="text-[#539E43] h-12" />
+            <PiIcon className="text-[#FFD43B] h-12" />
+            <BeanIcon className="text-[#5382A1] h-12" />
+            <CodepenIcon className="text-[#8892BF] h-12" />
+          </div>
+        </div>
+      </section> */}
+      <section className="text-center py-16">
+        <h2 className="text-5xl font-bold mb-12">Click, click, done.</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-8">
+          <Card className="">
+            <CardHeader>
+              <CardTitle>Choose your service type</CardTitle>
+              <CardDescription>Run web apps, background workers, static sites, cron jobs, and more.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              Test
+            </CardContent>
+            {/* <CardFooter>
+              <Button className="w-full">Get Plan</Button>
+            </CardFooter> */}
+          </Card>
+          <Card className="bg-[#25262b]">
+            <CardHeader>
+              <CardTitle>Deploy in seconds</CardTitle>
+              <CardDescription>Build and run your apps with lightning speed and flexible configs.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              Test2
+            </CardContent>
+            {/* <CardFooter>
+              <Button className="  w-full">Deploy</Button>
+            </CardFooter> */}
+          </Card>
+          <Card className="bg-[#25262b]">
+            <CardHeader>
+              <CardTitle>Update automatically</CardTitle>
+              <CardDescription>Stay current with your code thanks to seamless, automatic deploys.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              test 3
+            </CardContent>
+            {/* <CardFooter>
+              <Button className="  w-full">Activate</Button>
+            </CardFooter> */}
+          </Card>
+        </div>
+      </section>
+      <footer className=" py-12">
+        <div className="flex justify-center space-x-4">
+          <EqualIcon className=" h-6" />
+          <WavesIcon className=" h-6" />
+          <RocketIcon className=" h-6" />
+          <GithubIcon className=" h-6" />
+          <FlagIcon className=" h-6" />
+        </div>
+        <div className="text-center text-gray-400 mt-8">
+          <p>Trusted by 750,000+ developers and teams</p>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+function BeanIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10.165 6.598C9.954 7.478 9.64 8.36 9 9c-.64.64-1.521.954-2.402 1.165A6 6 0 0 0 8 22c7.732 0 14-6.268 14-14a6 6 0 0 0-11.835-1.402Z" />
+      <path d="M5.341 10.62a4 4 0 1 0 5.279-5.28" />
+    </svg>
+  )
+}
+
+
+function CodepenIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2" />
+      <line x1="12" x2="12" y1="22" y2="15.5" />
+      <polyline points="22 8.5 12 15.5 2 8.5" />
+      <polyline points="2 15.5 12 8.5 22 15.5" />
+      <line x1="12" x2="12" y1="2" y2="8.5" />
+    </svg>
+  )
+}
+
+
+function EqualIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="5" x2="19" y1="9" y2="9" />
+      <line x1="5" x2="19" y1="15" y2="15" />
+    </svg>
+  )
+}
+
+
+function FlagIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
+      <line x1="4" x2="4" y1="22" y2="15" />
+    </svg>
+  )
+}
+
+
+function GemIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M6 3h12l4 6-10 13L2 9Z" />
+      <path d="M11 3 8 9l4 13 4-13-3-6" />
+      <path d="M2 9h20" />
+    </svg>
+  )
+}
+
+
+function GithubIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+      <path d="M9 18c-4.51 2-5-2-7-2" />
+    </svg>
+  )
+}
+
+
+function LanguagesIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m5 8 6 6" />
+      <path d="m4 14 6-6 2-3" />
+      <path d="M2 5h12" />
+      <path d="M7 2h1" />
+      <path d="m22 22-5-10-5 10" />
+      <path d="M14 18h6" />
+    </svg>
+  )
+}
+
+
+function LogInIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+      <polyline points="10 17 15 12 10 7" />
+      <line x1="15" x2="3" y1="12" y2="12" />
+    </svg>
+  )
+}
+
+
+function PiIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="9" x2="9" y1="4" y2="20" />
+      <path d="M4 7c0-1.7 1.3-3 3-3h13" />
+      <path d="M18 20c-1.7 0-3-1.3-3-3V4" />
+    </svg>
+  )
+}
+
+
+function RocketIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z" />
+      <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
+      <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
+      <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
+    </svg>
+  )
+}
+
+
+function WavesIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M2 6c.6.5 1.2 1 2.5 1C7 7 7 5 9.5 5c2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+      <path d="M2 12c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+      <path d="M2 18c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+    </svg>
+  )
+}
