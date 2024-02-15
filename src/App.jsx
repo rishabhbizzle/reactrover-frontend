5252bb9585c95b6b4ff9141086a3b69b7226cfdc
rishabhbizzle <rishabhbizzle@gmail.com>
2024-02-15 23:30:26 +0530

    feat: more changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from './components/theme-provider'
 import { ModeToggle } from './components/mode-toggle'
 import Home from './pages/home'
 import LandingPage from './pages/landing-page'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,8 +13,10 @@ function App() {
   return (
     <ThemeProvider defaultTheme = "dark"
     storageKey = "react">
+      <Navbar/>
       <LandingPage/>
     {/* <Home /> */}
+    <Footer/>
     </ThemeProvider>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className=" py-12">
+      <div className="flex justify-center space-x-4"></div>
+      <div className="text-center text-gray-400 mt-8">
+        <p>Trusted by 750,000+ developers and teams</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
