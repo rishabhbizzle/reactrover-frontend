a2a45bcaecffe2b675b95b75db8b0659d92c5d64
rishabhbizzle <rishabhbizzle@gmail.com>
2024-02-16 21:54:28 +0530

    toaster added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
-import { ThemeProvider } from './components/theme-provider'
-import { ModeToggle } from './components/mode-toggle'
-import Home from './pages/home'
-import LandingPage from './pages/landing-page'
-import Navbar from './components/navbar'
-import Footer from './components/footer'
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+import Home from "./components/deploy";
+import LandingPage from "./pages/landing-page";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <ThemeProvider defaultTheme = "dark"
-    storageKey = "react">
-      <Navbar/>
-      <LandingPage/>
-    {/* <Home /> */}
-    <Footer/>
+    <ThemeProvider defaultTheme="dark" storageKey="react">
+      <Toaster />
+      <Navbar />
+      <LandingPage />
+      <Footer />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
